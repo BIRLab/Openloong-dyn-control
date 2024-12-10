9d4e8e4f86af28dce70f54fdba1b24b879141596
chinaheyu <810130242@qq.com>
2024-12-10 10:58:06 +0800

    do not generate scripts

--- hw_interface/scripts/generate_config.py
+++ hw_interface/scripts/generate_config.py
@@ -21,15 +21,9 @@ for node_name, node_config in bus_yaml['nodes'].items():
 with open(os.path.join(out_dir, 'bus.yml'), 'w') as f:
     f.write(yaml.dump(out_yaml))
 
-udev_rules = """# Copy this file to /etc/udev/rules.d/99-candlelight.rules and run the following commands:
-# sudo udevadm control --reload-rules && sudo systemctl restart systemd-udevd && sudo udevadm trigger
-"""
-
 motor_descriptions = []
 
 for bus_name, bus_config in bus_yaml['buses'].items():
-    serial_number = bus_config['serial_number']
-    udev_rules += f'\nSUBSYSTEM=="net", ATTRS{{idVendor}}=="1d50", ATTRS{{idProduct}}=="606f", ATTRS{{serial}}=="{serial_number}", NAME="{bus_name}"'
     for node in bus_config['nodes']:
         node_id = node['node_id']
         global_id = node['global_id']
@@ -37,9 +31,6 @@ for bus_name, bus_config in bus_yaml['buses'].items():
         offset = node['offset']
         reverse = 'true' if node['reverse'] else 'false'
         motor_descriptions.append(f'{{"{bus_name}", {node_id}, {global_id}, {encoder}, {offset}, {reverse}}},')
-
-with open(os.path.join(out_dir, '99-candlelight.rules'), 'w') as f:
-    f.write(udev_rules)
 
 with open(os.path.join(out_dir, 'motor_descriptions.h'), 'w') as f:
     f.write('\n'.join(motor_descriptions))
